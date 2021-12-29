dd5760f0f97f29e568918024229328edbb6cfde2
berubenic <berubenic@gmail.com>
2021-12-29 18:36:42 -0500

    refactor home js

--- src/contact.js
+++ src/contact.js
@@ -1,4 +1,6 @@
 import { Loader } from "@googlemaps/js-api-loader";
+import FacebookLogo from "./f_logo_RGB-Blue_100.png";
+import InstagramLogo from "./Instagram_Glyph_Gradient_RGB.png";
 
 const API_KEY = process.env.GOOGLE_API_KEY;
 
@@ -10,12 +12,54 @@ const loader = new Loader({
 export class Contact {
   constructor(container) {
     this.container = container;
+    this.contactContainer = document.createElement("div");
   }
 
   display() {
-    this.#displayAddress();
+    this.contactContainer.classList.add("contact-container");
+    this.container.appendChild(this.contactContainer);
+    this.#createDescription();
     this.#displayEmail();
+    this.#displaySocials();
+    this.#displayHours();
+    this.#displayAddress();
     this.#displayMap();
+  }
+
+  #displayHours() {
+    const hours = {
+      lundi: "11:30a.m.-8p.m.",
+      mardi: "11:30a.m.-8p.m.",
+      mercredi: "11:30a.m.-8p.m.",
+      jeudi: "11:30a.m.-9p.m.",
+      vendredi: "11:30a.m.-9p.m.",
+      samedi: "11:30a.m.-10p.m.",
+      dimanche: "11:30a.m.-8p.m.",
+    };
+    let hoursDiv = document.createElement("div");
+    let daysList = document.createElement("ul");
+    let hoursList = document.createElement("ul");
+    hoursDiv.appendChild(daysList);
+    hoursDiv.appendChild(hoursList);
+    hoursDiv.classList.add("contact-hours-container");
+    daysList.classList.add("contact-hours-day-list");
+    hoursList.classList.add("contact-hours-hours-list");
+    this.contactContainer.appendChild(hoursDiv);
+    for (let [days, time] of Object.entries(hours)) {
+      let dayElement = document.createElement("li");
+      let timeElement = document.createElement("li");
+      dayElement.innerHTML = days;
+      timeElement.innerHTML = time;
+      daysList.appendChild(dayElement);
+      hoursList.appendChild(timeElement);
+    }
+  }
+
+  #createDescription() {
+    let text = "Laissez nous un commentaire!";
+    let p = document.createElement("p");
+    p.innerHTML = text;
+    this.contactContainer.appendChild(p);
   }
 
   #displayAddress() {
@@ -26,7 +70,8 @@ export class Contact {
     let streetP = document.createElement("p");
     let cityP = document.createElement("p");
     let phoneNumberP = document.createElement("p");
-    this.container.appendChild(addressDiv);
+    this.contactContainer.appendChild(addressDiv);
+    addressDiv.classList.add("contact-address-container");
     addressDiv.appendChild(streetP);
     addressDiv.appendChild(cityP);
     addressDiv.appendChild(phoneNumberP);
@@ -37,17 +82,54 @@ export class Contact {
 
   #displayEmail() {
     let email = "info@lavictorienne.ca";
-    let emailDiv = document.createElement("div");
-    let emailP = document.createElement("p");
+    let emailP = document.createElement("a");
+    emailP.classList.add("contact-email");
+    emailP.setAttribute("href", "mailto:info@lavictorienne.ca");
     emailP.innerHTML = email;
-    emailDiv.appendChild(emailP);
-    this.container.appendChild(emailDiv);
+    this.contactContainer.appendChild(emailP);
+  }
+
+  #displaySocials() {
+    let socialDiv = document.createElement("div");
+    socialDiv.classList.add("contact-social-container");
+    this.contactContainer.appendChild(socialDiv);
+    this.#createFBSocial(socialDiv);
+    this.#createIGSocial(socialDiv);
+  }
+
+  #createFBSocial(socialDiv) {
+    let anchor = document.createElement("a");
+    let image = document.createElement("img");
+    image.src = FacebookLogo;
+    image.classList.add("logo");
+
+    anchor.setAttribute(
+      "href",
+      "https://www.facebook.com/LaVictorienneLavaltrie/?fref=ts"
+    );
+    anchor.setAttribute("target", "_blank");
+    anchor.appendChild(image);
+    socialDiv.appendChild(anchor);
+  }
+
+  #createIGSocial(socialDiv) {
+    let anchor = document.createElement("a");
+    let image = document.createElement("img");
+    image.src = InstagramLogo;
+    image.classList.add("logo");
+    anchor.setAttribute(
+      "href",
+      "https://www.instagram.com/rotisserielavictorienne/"
+    );
+    anchor.setAttribute("target", "_blank");
+    anchor.appendChild(image);
+    socialDiv.appendChild(anchor);
   }
 
   #displayMap() {
     let mapDiv = document.createElement("div");
     mapDiv.setAttribute("id", "map");
-    this.container.appendChild(mapDiv);
+    this.contactContainer.appendChild(mapDiv);
     const restaurant = { lat: 45.882818879630626, lng: -73.27922154540192 };
     loader.load().then(() => {
       const map = new google.maps.Map(document.getElementById("map"), {
--- src/home.js
+++ src/home.js
@@ -7,55 +7,95 @@ export class Home {
   }
 
   display() {
-    this.container.appendChild(this.homeContainer);
-    this.homeContainer.classList.add("home-container");
-    this.#createDescription();
+    this.#createHomeContainer();
+    this.#createHeaders();
     this.#createImage();
+    this.#createSchedule();
     this.#addAddress();
   }
 
-  #createDescription() {
-    let descriptionContainer = document.createElement("div");
-    this.homeContainer.appendChild(descriptionContainer);
-    descriptionContainer.classList.add("home-description-container");
-    let topDescription = "Bien plus qu'une Rôtisserie!";
-    let bottomDescription = " Au plaisir de vous servir!";
-    let header = document.createElement("h2");
-    header.classList.add("home-top-description");
-    header.innerHTML = topDescription;
-    descriptionContainer.appendChild(header);
-    let smallerHeader = document.createElement("h3");
-    smallerHeader.classList.add("home-bottom-description");
-    smallerHeader.innerHTML = bottomDescription;
-    descriptionContainer.appendChild(smallerHeader);
+  #createHomeContainer() {
+    this.container.appendChild(this.homeContainer);
+    this.homeContainer.classList.add("home-container");
+  }
+
+  #createHeaders() {
+    // container
+    let headerContainer = document.createElement("div");
+    this.homeContainer.appendChild(headerContainer);
+    headerContainer.classList.add("home-header-container");
+    // first header with text
+    let firstHeader = document.createElement("h2");
+    let firstHeaderText = "Bien plus qu'une Rôtisserie!";
+    firstHeader.classList.add("home-first-header");
+    firstHeader.innerHTML = firstHeaderText;
+    headerContainer.appendChild(firstHeader);
+    // second header with text
+    let secondHeader = document.createElement("h3");
+    let secondHeaderText = " Au plaisir de vous servir!";
+    secondHeader.classList.add("home-second-header");
+    secondHeader.innerHTML = secondHeaderText;
+    headerContainer.appendChild(secondHeader);
   }
 
   #createImage() {
-    let image = new Image();
+    let image = document.createElement("img");
     image.src = frontViewImage;
     image.classList.add("home-image");
     this.homeContainer.appendChild(image);
   }
 
   #addAddress() {
+    // container
     let addressContainer = document.createElement("div");
     addressContainer.classList.add("home-address-container");
     this.homeContainer.appendChild(addressContainer);
+    let address = {
+      streetName: "1231 Notre-Dame",
+      cityProvincePostal: "Lavaltrie, QC J5T 1R7",
+      phoneNumber: "Tel.: 450-586-1337",
+    };
 
-    let firstLine = "1231 Notre-Dame";
-    let secondLine = "Lavaltrie, QC J5T 1R7";
-    let phoneNumber = "Tel.: 450-586-1337";
+    // insert each line of address as p tag in container
+    Object.entries(address).forEach(([key, value]) => {
+      let paragraph = document.createElement("p");
+      paragraph.innerHTML = value;
+      addressContainer.appendChild(paragraph);
+    });
+  }
 
-    let firstLineP = document.createElement("p");
-    let secondLineP = document.createElement("p");
-    let thirdLineP = document.createElement("p");
-
-    firstLineP.innerHTML = firstLine;
-    secondLineP.innerHTML = secondLine;
-    thirdLineP.innerHTML = phoneNumber;
-
-    addressContainer.appendChild(firstLineP);
-    addressContainer.appendChild(secondLineP);
-    addressContainer.appendChild(thirdLineP);
+  #createSchedule() {
+    const schedule = {
+      lundi: "11:30a.m.-8p.m.",
+      mardi: "11:30a.m.-8p.m.",
+      mercredi: "11:30a.m.-8p.m.",
+      jeudi: "11:30a.m.-9p.m.",
+      vendredi: "11:30a.m.-9p.m.",
+      samedi: "11:30a.m.-10p.m.",
+      dimanche: "11:30a.m.-8p.m.",
+    };
+    // container
+    let scheduleContainer = document.createElement("div");
+    scheduleContainer.classList.add("contact-hours-container");
+    this.homeContainer.appendChild(scheduleContainer);
+    // week days
+    let weekDaysList = document.createElement("ul");
+    scheduleContainer.appendChild(weekDaysList);
+    weekDaysList.classList.add("contact-week-days-list");
+    // opening and closing time
+    let timeList = document.createElement("ul");
+    scheduleContainer.appendChild(timeList);
+    timeList.classList.add("contact-time-list");
+    // insert each day and time from schedule in respective list
+    for (let [weekDay, time] of Object.entries(schedule)) {
+      // day
+      let dayElement = document.createElement("li");
+      dayElement.innerHTML = weekDay;
+      weekDaysList.appendChild(dayElement);
+      // time
+      let timeElement = document.createElement("li");
+      timeElement.innerHTML = time;
+      timeList.appendChild(timeElement);
+    }
   }
 }
